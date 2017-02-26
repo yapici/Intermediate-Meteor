4d5532ce4d9bc7281b4c3b2f0e4eea3757999aa8
Engin Yapici <enginyapici@gmail.com>
2017-02-25 22:27:48 -0600

    Intermediate Meteor Tutorial #18 - Update Methods & Adding To Menu
    https://www.youtube.com/watch?v=DuRcJS72Eqo

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -3,6 +3,9 @@ Recipes = new Mongo.Collection('recipes');
 Recipes.allow({
     insert: function (userId, doc) {
         return !!userId;
+    },
+    update: function(userId, doc) {
+        return !!userId;
     }
 });
 
@@ -55,6 +58,16 @@ RecipeSchema = new SimpleSchema({
             type: "hidden"
         }
     }
+});
+
+Meteor.methods({
+   toggleMenuItem: function (id, currentState) {
+       Recipes.update(id, {
+           $set: {
+               inMenu: !currentState
+           }
+       });
+   }
 });
 
 Recipes.attachSchema(RecipeSchema);
